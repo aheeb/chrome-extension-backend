dc138b546b3882f6b06f7f9bedec456083f0863c
aheeb <78415840+aheeb@users.noreply.github.com>
2024-03-25 11:35:48 +0100

    Update database password variable

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const pool = new Pool({
     user: process.env.DATABASE_USER,
     host: process.env.DATABASE_HOST,
     database: process.env.DATABASE_NAME,
-    password: process.env.DATABASE_PASSWORD,
+    password: process.env.DATABASE_PW,
     port: 5432, // Default PostgreSQL port
     // For Render or other cloud providers, you might need to add SSL settings
     ssl: {
